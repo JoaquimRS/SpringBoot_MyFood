b59f36e8fea5566263b1282b7dfbccfce765b469
JoaquimRS <joaquimdaweb@gmail.com>
2022-11-17 16:42:06 +0100

    Initial Structure

---
+++ src/main/java/com/myfood/springboot_myfood/SpringbootMyfoodApplication.java
@@ -0,0 +1,13 @@
+package com.myfood.springboot_myfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMyfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfood/springboot_myfood/SpringbootMyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfood.springboot_myfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
