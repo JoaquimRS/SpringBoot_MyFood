5405a13e0acf411e84f216a73ce2d7dcfd608d82
Moises Guerola <daw.moisesguerola@gmail.com>
2022-11-30 16:55:11 +0100

    (Backend) - BannedDays

--- src/main/java/com/myfood/springboot_myfood/domain/reserva/controller/ReservaController.java
+++ src/main/java/com/myfood/springboot_myfood/domain/reserva/controller/ReservaController.java
@@ -1,10 +1,13 @@
 package com.myfood.springboot_myfood.domain.reserva.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myfood.springboot_myfood.domain.reserva.repository.ReservaRepository;
@@ -16,7 +19,17 @@ public class ReservaController {
     ReservaRepository repository;
 
     @GetMapping
-    List<?> getHolidays() {
-        return this.repository.getHolidays();
+    List<Object> getHolidays(@RequestParam Integer comensales) {
+        List<Object> finalList = new ArrayList<Object>();
+
+        for(Object holiday : this.repository.getHolidays()) {
+            finalList.add(holiday);
+        }
+
+        for(Object bannedDay : this.repository.getBannedDays(comensales)) {
+            finalList.add(bannedDay);
+        }
+
+        return finalList;
     }
 }
--- src/main/java/com/myfood/springboot_myfood/domain/reserva/repository/ReservaRepository.java
+++ src/main/java/com/myfood/springboot_myfood/domain/reserva/repository/ReservaRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.myfood.springboot_myfood.domain.reserva.model.ReservaModel;
 
 public interface ReservaRepository extends JpaRepository<ReservaModel, Long> {
     @Query(value = "SELECT fecha FROM festivos", nativeQuery = true)
     public List<?> getHolidays();
+    
+    @Query(value = "SELECT r.fecha FROM reservas r GROUP BY r.fecha HAVING (SUM(r.n_comensales) + :comensales) > 50;", nativeQuery = true)
+    List<?> getBannedDays(@Param("comensales") Integer comensales);
 }
